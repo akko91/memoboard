03bfede2926fd81214fd557458c13fae8e8f1cb0
akko91 <seob91@gmail.com>
2020-11-09 20:29:24 +0900

    목록 불러오기
    임시로 모든 메모 불러오기

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from bson import objectid
 from flask import Flask, render_template, jsonify, request
 import requests
 from pymongo import MongoClient
@@ -12,40 +13,70 @@ db = client.dbsparta
 def home():
     return render_template('index.html')
 
+
 # 글쓰기
 @app.route('/memo', methods=['POST'])
 def post_memo():
     title = request.form['title']
     content = request.form['content']
     date = request.form['date']
+    writer = "글쓴이"
 
-    print(date);
+    # print(date);
 
     doc = {
         'title': title,
         'content': content,
-        # 'writer': 현재 작성자 명 받아와야됨
-        # 'date': 현재 시간 받아와야됨
+        'writer': writer,
+        'date': date
     }
-    
-    # db.memoboard.insert_one(doc);
+
+    db.memoboard.insert_one(doc);
 
     return jsonify({'result': 'success', 'msg': '메모 완료!'})
 
+
 # 목록 읽기
-@app.route('/memo-list', methods=['GET'])
+@app.route('/memolist', methods=['GET'])
 def get_memolist():
-    return jsonify({'result': 'success', 'msg': '목록 불러오기 완료!'})
+    memo_list = list(db.memoboard.find({}, {"_id": False}).sort('_id', -1))
+    id = list(db.memoboard.find({}, {"title": False, "content": False, "writer": False, "date": False}).sort('_id', -1))
+    # print(memo_list)
+
+    id_list = [str(i["_id"]) for i in id]
+    # print(id_list)
+
+    memo_json = {"response": "success", "memoList": memo_list, "memoId": id_list}
+
+    return jsonify(memo_json)
+
 
 # 메모 읽기
-@app.route('/memo', methods=['GET'])
+@app.route('/memo_one', methods=['POST'])
 def get_memo():
+    # id = request.form['id']
+    writer = request.form['writer']
+    date = request.form['date']
+
+    # print(writer, date)
+
+    memo = list(db.memoboard.find({'date': date}, {'_id': False}))
+    # memo db.memoboard.find({ $and: [{'writer': writer}, {'date': date} ] })
+
+    # memo = list(db.memoboard.find({"_id":ObjectId(id)})) 오브젝트 아이디로 검색하는 방법 찾지못함.
+
+
+    print(memo)
+
+
     return jsonify({'result': 'success', 'msg': '메모 불러오기 완료!'})
+
 
 # 삭제
 @app.route('/memo', methods=['DELETE'])
 def delete_memo():
     return jsonify({'result': 'success', 'msg': '메모 삭제 완료!'})
+
 
 # 수정
 @app.route('/memo', methods=['PUT'])
